c790437a1d952230e0de1c304e343212f15f032d
Trust_04zh <trust04zh@gmail.com>
2022-05-30 23:00:54 +0800

    stack migrates to 0x7fffffff

--- src/riscv_regfile.v
+++ src/riscv_regfile.v
@@ -20,14 +20,42 @@ module riscv_regfile (
 
 reg [31:0] register[0:31];
 
-integer i_1;
-
 // register write
 always @(negedge clk or posedge rst) begin
     if (rst == 1) begin
-        for (i_1 = 0; i_1 < 32; i_1 = i_1 + 1) begin
-            register[i_1] <= 32'h0000_0000; 
-        end
+        register[0] <= 32'h0000_0000;
+        register[1] <= 32'h0000_0000;
+        register[2] <= 32'h7fff_ffff;
+        register[3] <= 32'h0000_0000;
+        register[4] <= 32'h0000_0000;
+        register[5] <= 32'h0000_0000;
+        register[6] <= 32'h0000_0000;
+        register[7] <= 32'h0000_0000;
+        register[8] <= 32'h0000_0000;
+        register[9] <= 32'h0000_0000;
+        register[10] <= 32'h0000_0000;
+        register[11] <= 32'h0000_0000;
+        register[12] <= 32'h0000_0000;
+        register[13] <= 32'h0000_0000;
+        register[14] <= 32'h0000_0000;
+        register[15] <= 32'h0000_0000;
+        register[16] <= 32'h0000_0000;
+        register[17] <= 32'h0000_0000;
+        register[18] <= 32'h0000_0000;
+        register[19] <= 32'h0000_0000;
+        register[20] <= 32'h0000_0000;
+        register[21] <= 32'h0000_0000;
+        register[22] <= 32'h0000_0000;
+        register[23] <= 32'h0000_0000;
+        register[24] <= 32'h0000_0000;
+        register[25] <= 32'h0000_0000;
+        register[26] <= 32'h0000_0000;
+        register[27] <= 32'h0000_0000;
+        register[28] <= 32'h0000_0000;
+        register[29] <= 32'h0000_0000;
+        register[30] <= 32'h0000_0000;
+        register[31] <= 32'h0000_0000;
+        register[32] <= 32'h0000_0000;
     end
     else if (reg_write_en == 1 && !(rd == 0)) begin
         register[rd] = data_to_reg;
