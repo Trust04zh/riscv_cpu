1184563407ad0bb510f70c9cae2b3922d5aec2ad
Frank Wu <56958034+GhostFrankWu@users.noreply.github.com>
2022-06-05 20:41:38 +0800

    update compile script

--- script/cross-compile-rv.sh
+++ script/cross-compile-rv.sh
@@ -1,11 +1,7 @@
 cd .
 PREFIX=test
 TOOLCHAIN=/opt/riscv/bin/riscv32-unknown-linux-gnu
-# python3 corss-compile-to-asm.py ${PREFIX}.c > ${PREFIX}.asm
-${TOOLCHAIN}-gcc -c ${PREFIX}.c -o ${PREFIX}.o
-${TOOLCHAIN}-ld --Ttext 0x0 -Tdata 0x10000 -nostdlib ${PREFIX}.o -o ${PREFIX}.out
-${TOOLCHAIN}-strip -R .reginfo ${PREFIX}.out
-${TOOLCHAIN}-strip -R .riscv.abiflags ${PREFIX}.out
-${TOOLCHAIN}-strip -R .gnu.attributes ${PREFIX}.out
+${TOOLCHAIN}-gcc -march=rv32i -c ${PREFIX}.c -o ${PREFIX}.o
+${TOOLCHAIN}-ld --Ttext 0x0 -Tdata 0x10000 ${PREFIX}.o -o ${PREFIX}.out
 ${TOOLCHAIN}-objcopy -O binary ${PREFIX}.out ${PREFIX}.bin
 python3 convert-pg-coe.py ${PREFIX}.bin
